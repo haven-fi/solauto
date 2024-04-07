8c686625f38d05a4342029c33e87155578c4b3da
Chelioso <cheliosooo26@gmail.com>
2024-04-07 16:41:23 -0400

    fixes

--- programs/solauto/src/constants.rs
+++ programs/solauto/src/constants.rs
@@ -7,17 +7,15 @@ pub const SOLEND_PROGRAM: Pubkey = Pubkey::new_from_array([
 ]);
 
 // MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA
-// TODO
 pub const MARGINFI_PROGRAM: Pubkey = Pubkey::new_from_array([
-    6, 155, 139, 152, 90, 171, 83, 42, 69, 9, 13, 232, 85, 127, 205, 220, 190, 108, 183, 239, 199, 58,
-    10, 101, 176, 111, 146, 3, 93, 183, 62, 236,
+    5, 48, 122, 214, 69, 75, 188, 94, 30, 78, 146, 5, 146, 83, 161, 139, 184, 200, 134, 140, 88, 166,
+    49, 46, 200, 106, 57, 230, 34, 78, 55, 59,
 ]);
 
 // KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD
-// TODO
 pub const KAMINO_PROGRAM: Pubkey = Pubkey::new_from_array([
-    6, 155, 139, 152, 90, 171, 83, 42, 69, 9, 13, 232, 85, 127, 205, 220, 190, 108, 183, 239, 199, 58,
-    10, 101, 176, 111, 146, 3, 93, 183, 62, 236,
+    4, 178, 172, 177, 18, 88, 204, 227, 104, 44, 65, 139, 168, 114, 255, 61, 249, 17, 2, 113, 47, 21,
+    175, 18, 182, 190, 105, 179, 67, 91, 0, 8,
 ]);
 
 // AprYCPiVeKMCgjQ2ZufwChMzvQ5kFjJo2ekTLSkXsQDm
