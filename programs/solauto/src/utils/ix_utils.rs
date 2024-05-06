0bbc354ec217b8db8fe3bc94340ef86e83119536
Chelioso <cheliosooo26@gmail.com>
2024-05-06 01:11:04 -0400

    wip

--- programs/solauto/src/utils/ix_utils.rs
+++ programs/solauto/src/utils/ix_utils.rs
@@ -1,22 +1,26 @@
 use borsh::BorshSerialize;
 use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, instruction::Instruction, program::invoke,
+    account_info::AccountInfo,
+    entrypoint::ProgramResult,
+    instruction::Instruction,
+    program::invoke,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    sysvar::instructions::load_instruction_at_checked,
 };
 
-use super::solana_utils::invoke_signed_with_seed;
-use crate::types::shared::{DeserializedAccount, PositionAccount};
+use super::solana_utils::{ invoke_signed_with_seed, get_anchor_ix_discriminator };
+use crate::types::shared::{ DeserializedAccount, PositionAccount };
 
 pub fn update_data<T: BorshSerialize>(account: &mut DeserializedAccount<T>) -> ProgramResult {
-    account
-        .data
-        .serialize(&mut &mut account.account_info.data.borrow_mut()[..])?;
+    account.data.serialize(&mut &mut account.account_info.data.borrow_mut()[..])?;
     Ok(())
 }
 
 pub fn invoke_instruction(
     instruction: Instruction,
     account_infos: &[AccountInfo],
-    solauto_position: &DeserializedAccount<PositionAccount>,
+    solauto_position: &DeserializedAccount<PositionAccount>
 ) -> ProgramResult {
     if solauto_position.data.self_managed {
         invoke(&instruction, account_infos)?;
@@ -24,11 +28,81 @@ pub fn invoke_instruction(
         invoke_signed_with_seed(
             &instruction,
             account_infos,
-            vec![
-                &[solauto_position.data.position_id],
-                solauto_position.data.authority.as_ref(),
-            ],
+            vec![&[solauto_position.data.position_id], solauto_position.data.authority.as_ref()]
         )?;
     }
     Ok(())
+}
+
+pub fn get_relative_instruction(
+    ixs_sysvar: &AccountInfo,
+    current_ix_idx: u16,
+    relative_idx: i16,
+    total_ix_in_tx: u16
+) -> Result<Option<Instruction>, ProgramError> {
+    if
+        (current_ix_idx as i16) + relative_idx > 0 &&
+        (current_ix_idx as i16) + relative_idx < (total_ix_in_tx as i16)
+    {
+        Ok(
+            Some(
+                load_instruction_at_checked(
+                    ((current_ix_idx as i16) + relative_idx) as usize,
+                    ixs_sysvar
+                )?
+            )
+        )
+    } else {
+        Ok(None)
+    }
+}
+
+pub struct InstructionChecker {
+    program_id: Pubkey,
+    ix_discriminators: Option<Vec<u64>>,
+}
+impl InstructionChecker {
+    pub fn from(program_id: Pubkey, ix_discriminators: Option<Vec<u64>>) -> Self {
+        Self {
+            program_id,
+            ix_discriminators,
+        }
+    }
+    pub fn from_anchor(program_id: Pubkey, namespace: &str, ix_names: Vec<&str>) -> Self {
+        let mut ix_discriminators: Vec<u64> = Vec::new();
+        for name in ix_names.iter() {
+            ix_discriminators.push(get_anchor_ix_discriminator(namespace, name));
+        }
+        Self {
+            program_id,
+            ix_discriminators: Some(ix_discriminators),
+        }
+    }
+    pub fn matches(&self, ix: &Option<Instruction>) -> bool {
+        if ix.is_none() {
+            return false;
+        }
+
+        let instruction = ix.as_ref().unwrap();
+        if instruction.program_id == self.program_id {
+            if instruction.data.len() >= 8 {
+                let discriminator: [u8; 8] = instruction.data[0..8]
+                    .try_into()
+                    .expect("Slice with incorrect length");
+
+                if
+                    self.ix_discriminators.is_none() ||
+                    self.ix_discriminators
+                        .as_ref()
+                        .unwrap()
+                        .iter()
+                        .any(|&x| x == u64::from_le_bytes(discriminator))
+                {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
 }
--- programs/solauto/src/utils/solauto_utils.rs
+++ programs/solauto/src/utils/solauto_utils.rs
@@ -2,7 +2,7 @@ use solana_program::{
     account_info::AccountInfo,
     clock::Clock,
     entrypoint::ProgramResult,
-    instruction::{ get_stack_height, Instruction, TRANSACTION_LEVEL_STACK_HEIGHT },
+    instruction::{ get_stack_height, TRANSACTION_LEVEL_STACK_HEIGHT },
     msg,
     program_error::ProgramError,
     program_pack::Pack,
@@ -13,12 +13,9 @@ use spl_associated_token_account::get_associated_token_address;
 use spl_token::state::Account as TokenAccount;
 use std::ops::{ Add, Mul };
 
-use super::solana_utils::{
-    self,
-    account_has_custom_data,
-    get_anchor_ix_discriminator,
-    init_ata_if_needed,
-    init_new_account,
+use super::{
+    ix_utils::{ get_relative_instruction, InstructionChecker },
+    solana_utils::{ self, account_has_custom_data, init_ata_if_needed, init_new_account },
 };
 use crate::{
     constants::{
@@ -362,79 +359,6 @@ pub fn get_rebalance_step(
         Ok(SolautoRebalanceStep::FinishSolautoRebalanceSandwich)
     } else {
         Err(SolautoError::IncorrectInstructions.into())
-    }
-}
-
-pub fn get_relative_instruction(
-    ixs_sysvar: &AccountInfo,
-    current_ix_idx: u16,
-    relative_idx: i16,
-    total_ix_in_tx: u16
-) -> Result<Option<Instruction>, ProgramError> {
-    if
-        (current_ix_idx as i16) + relative_idx > 0 &&
-        (current_ix_idx as i16) + relative_idx < (total_ix_in_tx as i16)
-    {
-        Ok(
-            Some(
-                load_instruction_at_checked(
-                    ((current_ix_idx as i16) + relative_idx) as usize,
-                    ixs_sysvar
-                )?
-            )
-        )
-    } else {
-        Ok(None)
-    }
-}
-
-pub struct InstructionChecker {
-    program_id: Pubkey,
-    ix_discriminators: Option<Vec<u64>>,
-}
-impl InstructionChecker {
-    pub fn from(program_id: Pubkey, ix_discriminators: Option<Vec<u64>>) -> Self {
-        Self {
-            program_id,
-            ix_discriminators,
-        }
-    }
-    pub fn from_anchor(program_id: Pubkey, namespace: &str, ix_names: Vec<&str>) -> Self {
-        let mut ix_discriminators: Vec<u64> = Vec::new();
-        for name in ix_names.iter() {
-            ix_discriminators.push(get_anchor_ix_discriminator(namespace, name));
-        }
-        Self {
-            program_id,
-            ix_discriminators: Some(ix_discriminators),
-        }
-    }
-    pub fn matches(&self, ix: &Option<Instruction>) -> bool {
-        if ix.is_none() {
-            return false;
-        }
-
-        let instruction = ix.as_ref().unwrap();
-        if instruction.program_id == self.program_id {
-            if instruction.data.len() >= 8 {
-                let discriminator: [u8; 8] = instruction.data[0..8]
-                    .try_into()
-                    .expect("Slice with incorrect length");
-
-                if
-                    self.ix_discriminators.is_none() ||
-                    self.ix_discriminators
-                        .as_ref()
-                        .unwrap()
-                        .iter()
-                        .any(|&x| x == u64::from_le_bytes(discriminator))
-                {
-                    return true;
-                }
-            }
-        }
-
-        false
     }
 }
 
