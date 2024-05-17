1fd4451ccf5f00a6796c325f293b005ae029c17a
Chelioso <cheliosooo26@gmail.com>
2024-05-17 00:36:36 -0400

    wip

--- programs/solauto-sdk/tests/claim_referral_fees.rs
+++ programs/solauto-sdk/tests/claim_referral_fees.rs
@@ -54,6 +54,89 @@ mod claim_referral_fees {
             .general.create_referral_state_accounts().await
             .unwrap();
 
-        // TODO
+        let tx = Transaction::new_signed_with_payer(
+            &[data.general.claim_referral_fees_ix().signer(temp_account.pubkey()).instruction()],
+            Some(&temp_account.pubkey()),
+            &[&temp_account],
+            data.general.ctx.last_blockhash
+        );
+        let err = data.general.ctx.banks_client.process_transaction(tx).await.unwrap_err();
+        assert_instruction_error!(err, InstructionError::Custom(0));
+    }
+
+    #[tokio::test]
+    async fn incorrect_signer_referral_state() {
+        let args = GeneralArgs::new();
+
+        let mut data = MarginfiTestData::new(&args).await;
+        data.general.create_referral_state_accounts().await.unwrap();
+
+        let fake_referral_state_account = data.general.signer_referral_state;
+
+        let mut data = MarginfiTestData::new(&args).await;
+        data
+            .test_prefixtures().await
+            .unwrap()
+            .general.create_referral_state_accounts().await
+            .unwrap();
+        data.general
+            .create_ata(
+                data.general.ctx.payer.pubkey(),
+                data.general.referral_fees_dest_mint
+            ).await
+            .unwrap();
+        data.general
+            .create_ata(
+                data.general.signer_referral_state,
+                data.general.referral_fees_dest_mint
+            ).await
+            .unwrap();
+
+        // Try claim referral fees on data1's referral state
+        let err = data.general
+            .execute_instructions(
+                vec![
+                    data.general
+                        .claim_referral_fees_ix()
+                        .referral_state(fake_referral_state_account)
+                        .instruction()
+                ],
+                None
+            ).await
+            .unwrap_err();
+        assert_instruction_error!(err, InstructionError::Custom(0));
+    }
+
+    #[tokio::test]
+    async fn incorrect_token_accounts() {
+        let args = GeneralArgs::new();
+        let mut data = MarginfiTestData::new(&args).await;
+        data.test_prefixtures().await
+            .unwrap()
+            .general.create_referral_state_accounts().await
+            .unwrap();
+        data.general
+            .create_ata(data.general.ctx.payer.pubkey(), data.general.referral_fees_dest_mint).await
+            .unwrap();
+
+        let temp_wallet = Keypair::new().pubkey();
+        let fake_dest_ta = get_associated_token_address(
+            &temp_wallet,
+            &data.general.referral_fees_dest_mint.pubkey()
+        );
+        data.general.create_ata(temp_wallet, data.general.referral_fees_dest_mint).await.unwrap();
+
+        let err = data.general
+            .execute_instructions(
+                vec![
+                    data.general
+                        .claim_referral_fees_ix()
+                        .referral_fees_dest_ta(fake_dest_ta)
+                        .instruction()
+                ],
+                None
+            ).await
+            .unwrap_err();
+        assert_instruction_error!(err, InstructionError::Custom(0));
     }
 }
