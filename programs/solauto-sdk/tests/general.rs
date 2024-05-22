8eec2257f13f43d4ef1881dcce32d3fe8004d1d6
Chelioso <cheliosooo26@gmail.com>
2024-05-22 12:57:25 -0400

    wip

--- programs/solauto-sdk/tests/general.rs
+++ programs/solauto-sdk/tests/general.rs
@@ -4,19 +4,15 @@ pub mod test_utils;
 mod general {
     use solana_program_test::tokio;
     use solana_sdk::{
-        instruction::InstructionError,
-        pubkey::Pubkey,
-        rent::Rent,
-        signer::Signer,
-        system_instruction,
-        transaction::Transaction,
+        instruction::InstructionError, program_pack::Pack, pubkey::Pubkey, rent::Rent, signer::Signer, system_instruction, transaction::Transaction
     };
     use spl_associated_token_account::get_associated_token_address;
+    use spl_token::state::Account as TokenAccount;
 
     use crate::{ assert_instruction_error, test_utils::* };
 
     #[tokio::test]
-    async fn test_solauto_position_dos_attack() {
+    async fn test_solauto_position_attack() {
         let args = GeneralArgs::new();
         let mut data = MarginfiTestData::new(&args).await;
         data.test_prefixtures().await
@@ -43,6 +39,44 @@ mod general {
         data.general.ctx.banks_client.process_transaction(tx).await.unwrap();
 
         data.open_position(Some(data.general.default_setting_params.clone()), None).await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_ata_attack() {
+        let args = GeneralArgs::new();
+        let mut data = MarginfiTestData::new(&args).await;
+        data.test_prefixtures().await
+            .unwrap()
+            .general.create_referral_state_accounts().await
+            .unwrap();
+
+        let rent = Rent::default();
+        let space = 2000;
+
+        // Prefund ata with lamports
+        let tx = Transaction::new_signed_with_payer(
+            &[
+                system_instruction::transfer(
+                    &data.general.ctx.payer.pubkey(),
+                    &data.general.position_supply_liquidity_ta,
+                    rent.minimum_balance(space)
+                ),
+            ],
+            Some(&data.general.ctx.payer.pubkey()),
+            &[&data.general.ctx.payer],
+            data.general.ctx.last_blockhash
+        );
+        data.general.ctx.banks_client.process_transaction(tx).await.unwrap();
+
+        data.open_position(Some(data.general.default_setting_params.clone()), None).await.unwrap();
+
+        
+        let account = data.general.ctx.banks_client.get_account(data.general.position_supply_liquidity_ta).await.unwrap();
+        assert!(account.is_some());
+        let position_supply_liquidity_ta = TokenAccount::unpack(&mut account.unwrap().data.as_slice()).unwrap();
+
+        assert!(position_supply_liquidity_ta.owner == data.general.solauto_position);
+        assert!(position_supply_liquidity_ta.mint == data.general.supply_liquidity_mint.pubkey());
     }
 
     #[tokio::test]
