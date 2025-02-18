00148db2396627b5083952d2b57fd2eef0b3a3c2
Chelioso <cheliosooo26@gmail.com>
2025-02-18 03:17:07 -0500

    fix

--- solauto-sdk/src/utils/jupiterUtils.ts
+++ solauto-sdk/src/utils/jupiterUtils.ts
@@ -119,7 +119,7 @@ export async function getJupSwapTransaction(
     consoleLog("Raw inAmount:", quoteResponse.inAmount);
     const inc = Math.max(
       fromBps(finalPriceImpactBps) * 1.1,
-      fromBps(finalPriceSlippageBps) * 0.05
+      fromBps(finalPriceSlippageBps) * 0.03
     );
     consoleLog("Inc:", inc);
     quoteResponse.inAmount = Math.round(
