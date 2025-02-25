71dbc7791287c3983c769aa66229a2694478b0a6
Chelioso <cheliosooo26@gmail.com>
2025-02-25 15:52:20 -0500

    temp

--- solauto-sdk/src/utils/jupiterUtils.ts
+++ solauto-sdk/src/utils/jupiterUtils.ts
@@ -60,6 +60,7 @@ export async function getJupSwapTransaction(
     TOKEN_INFO[swapDetails.inputMint.toString()].isMeme ||
     TOKEN_INFO[swapDetails.outputMint.toString()].isMeme;
 
+  // TEMP REVERT ME
   const usingPyth =
     swapDetails.inputMint.equals(new PublicKey(PYTH)) ||
     swapDetails.outputMint.equals(new PublicKey(PYTH));
@@ -76,7 +77,7 @@ export async function getJupSwapTransaction(
             ? "ExactIn"
             : undefined,
         slippageBps: memecoinSwap ? 500 : 200,
-        maxAccounts: !swapDetails.exactOut ? (usingPyth ? 20 : 40) : undefined,
+        maxAccounts: !swapDetails.exactOut ? (usingPyth ? 15 : 40) : undefined,
       }),
     4,
     200
