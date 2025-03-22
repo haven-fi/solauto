71f15e193d45967e896409578092beb5d3ac074d
Chelioso <cheliosooo26@gmail.com>
2025-03-22 02:48:17 -0400

    wip

--- programs/solauto/src/rebalance/rebalancer_tests.rs
+++ programs/solauto/src/rebalance/rebalancer_tests.rs
@@ -2,13 +2,23 @@ use solana_program::pubkey::Pubkey;
 use spl_associated_token_account::get_associated_token_address;
 
 use crate::{
-    state::solauto_position::{ PositionData, PositionState, SolautoPosition },
+    constants::SOLAUTO_FEES_WALLET,
+    state::solauto_position::{
+        PositionData,
+        PositionState,
+        SolautoPosition,
+        SolautoSettingsParameters,
+        SolautoSettingsParametersInp,
+    },
     types::{
         instruction::RebalanceSettings,
-        shared::{ PositionType, RefreshedTokenState },
+        shared::{ PositionType, RebalanceDirection, RefreshedTokenState },
         solauto::PositionValues,
     },
-    utils::{ math_utils::to_base_unit, solauto_utils::{ update_token_state, SolautoFeesBps } },
+    utils::{
+        math_utils::{ from_bps, get_liq_utilization_rate_bps, to_base_unit },
+        solauto_utils::{ update_token_state, SolautoFeesBps },
+    },
 };
 
 use super::rebalancer::{ Rebalancer, RebalancerData, SolautoPositionData, TokenAccountData };
@@ -23,13 +33,22 @@ pub struct FakeRebalance {
     position_supply_ta_balance: Option<u64>,
     position_debt_ta_balance: Option<u64>,
     target_liq_utilization_rate_bps: Option<u16>,
+    settings: SolautoSettingsParametersInp,
+    rebalance_direction: RebalanceDirection,
 }
 
-fn create_rebalancer(
+fn create_rebalancer<'a>(
     data: FakeRebalance,
     rebalance_args: RebalanceSettings
-) -> (SolautoPosition, Rebalancer) {
+) -> (SolautoPosition, Rebalancer<'a>) {
     let mut state = PositionState::default();
+    state.max_ltv_bps = 6400;
+    state.liq_threshold_bps = 8181;
+    state.liq_utilization_rate_bps = get_liq_utilization_rate_bps(
+        data.pos.supply_usd,
+        data.pos.debt_usd,
+        from_bps(state.liq_threshold_bps)
+    );
 
     update_token_state(
         &mut state.supply,
@@ -54,13 +73,17 @@ fn create_rebalancer(
         })
     );
 
+    let mut position_data = PositionData::default();
+    position_data.setting_params = SolautoSettingsParameters::from(data.settings);
+
     let position = SolautoPosition::new(
         1,
         data.position_authority,
         PositionType::Leverage,
-        PositionData::default(),
+        position_data,
         state
     );
+
     let position_supply_ta = get_associated_token_address(&position.pubkey(), &data.supply_mint);
     let position_debt_ta = get_associated_token_address(&position.pubkey(), &data.debt_mint);
     let position_authority_supply_ta = get_associated_token_address(
@@ -71,6 +94,14 @@ fn create_rebalancer(
         &data.position_authority,
         &data.debt_mint
     );
+
+    let fees_mint = if data.rebalance_direction == RebalanceDirection::Boost {
+        data.supply_mint
+    } else {
+        data.debt_mint
+    };
+    let solauto_fees_ta = get_associated_token_address(&SOLAUTO_FEES_WALLET, &fees_mint);
+
     let rebalancer = Rebalancer::new(RebalancerData {
         rebalance_args,
         solauto_position: SolautoPositionData {
@@ -93,6 +124,8 @@ fn create_rebalancer(
         ),
         referred_by_state: None,
         referred_by_ta: None,
+        intermediary_ta: Pubkey::default(),
+        solauto_fees_ta,
     });
 
     (position, rebalancer)
